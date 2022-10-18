66782c26d8eb14aa3733819971a204902e558465
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2022-10-18 23:31:47 +0530

    implement custom tab bar

--- BookNow/ContentView.swift
+++ BookNow/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentTab:Tab = .home
+    init(){
+        UITabBar.appearance().isHidden = true
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+        VStack(spacing:0.0){
+            TabView(selection: $currentTab) {
+                
+                Text("Home")
+                    .tag(Tab.home)
+                Text("Location")
+                    .tag(Tab.location)
+                
+                TicketView()
+                    .tag(Tab.ticket)
+                
+                Text("Category")
+                    .tag(Tab.category)
+                
+                Text("Profile")
+                    .tag(Tab.profile)
+                
+            }
+            CustomTabBar(currentTab: $currentTab)
+        }
     }
 }
 
---
+++ BookNow/Model/TabData.swift
@@ -0,0 +1,16 @@
+//
+//  TabData.swift
+//  BookNow
+//
+//  Created by Dalveer singh on 18/10/22.
+//
+
+import Foundation
+
+enum Tab:String,CaseIterable{
+    case home = "Home"
+    case location = "Location"
+    case ticket = "Ticket"
+    case category = "Category"
+    case profile = "Profile"
+}
